abfd82a97dedeb9d5f65a427f037db9249718b31
ZhanYuSheng <1143109382@qq.com>
2019-11-04 11:49:33 +0800

    添加基础类

---
+++ src/main/java/com/eatshit/bolg/controller/BlogController.java
@@ -0,0 +1,9 @@
+package com.eatshit.bolg.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blog")
+public class BlogController {
+}
---
+++ src/main/java/com/eatshit/bolg/service/BlogServiceImpl.java
@@ -0,0 +1,7 @@
+package com.eatshit.bolg.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlogServiceImpl implements IBlogService {
+}
