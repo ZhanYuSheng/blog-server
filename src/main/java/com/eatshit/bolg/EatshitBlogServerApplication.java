3b048b86c439b692bff4a8636160a4104dd4c47b
ZhanYuSheng <1143109382@qq.com>
2019-11-04 11:42:17 +0800

    first commit

---
+++ src/main/java/com/eatshit/bolg/EatshitBlogServerApplication.java
@@ -0,0 +1,13 @@
+package com.eatshit.bolg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatshitBlogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatshitBlogServerApplication.class, args);
+    }
+
+}
